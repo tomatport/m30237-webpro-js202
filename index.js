a18fbf861aa51cd0605f402622b34a037483afb2
Thomas Robinson <git@tomr.me>
2025-02-05 11:29:36 +0000

    Qs 1-4

--- index.js
+++ index.js
@@ -1,20 +1,28 @@
-"use strict";
+/* eslint-disable no-unused-vars */
+'use strict';
 
+// 1. Easy Start: Complete the 'easyStart' function so that it always returns true.
 function easyStart() {
-  // add your code here...
-  return false;
+  return true;
 }
 
+// 2. Hello World: Complete the 'helloWorld' function so that so that it always returns the string "hello world".
 function helloWorld() {
-  // add your code here...
+  return 'hello world';
 }
 
+// 3. Numbers To Array: Change the 'numToArray' function so that instead of creating an array of numbers from 0 to 'limit', it creates an array of numbers counting down from 'limit' to 1.
 function numToArray(limit) {
-  let result = [];
-  for (let i=0; i<=limit; i=i+1) {
-    result.push(i)
+  const funNumbers = [];
+
+  for (let i = limit; i >= 1; i--) {
+    funNumbers.push(i);
   }
-  return result;
+
+  return funNumbers;
 }
 
-// add your functions here...
+// 4. Hello World 2: Create the function 'hw2' which returns an array of the strings "hello" and "world".
+function hw2() {
+  return ['hello', 'world'];
+}
